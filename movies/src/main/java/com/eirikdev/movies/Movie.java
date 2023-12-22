51f6419b6295a694492a117969f3d6c486dbd280
EirikBergesen <eirikbr@yahoo.com>
2023-12-23 00:58:17 +0100

    Added review class, and relationship to movie class

--- movies/src/main/java/com/eirikdev/movies/Movie.java
+++ movies/src/main/java/com/eirikdev/movies/Movie.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -23,5 +24,15 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    // Without this annotation, this would be an embedded relationship.
+    // All related reviews would be added to this list, and that would be fine.
+    // This allows for a one movie to many reviews relationship
+
+    // Manual reference relationship
+    // This will cause the database to store only the IDs of the review.
+    // and the reviews will be in a separate collection
+    // Should probably read about how one can use mongoDB and spring for other db relationships.
+    @DocumentReference
+    private List<Review> reviewIds;
 
 }
