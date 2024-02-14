d457872f7d4ff6033274d3670733758e5618079d
EirikBergesen <eirikbr@yahoo.com>
2024-02-14 11:18:41 +0100

    copy paste from repo, still does not work

--- react_movie_client/movie-client-v1/src/App.js
+++ react_movie_client/movie-client-v1/src/App.js
@@ -1,63 +1,73 @@
 import './App.css';
 import api from './api/axiosConfig';
-import { useEffect, useState } from 'react';
+import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
-import { Routes, Route } from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
 import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
   const [movies, setMovies] = useState();
   const [movie, setMovie] = useState();
-  const [reviews, setReviews] = useState();
+  const [reviews, setReviews] = useState([]);
 
-  const getMovies = async () => {
-    try {
+  const getMovies = async () =>{
+    
+    try
+    {
+
       const response = await api.get("/api/v1/movies");
-      setMovies(response.data);
-    } catch (error) {
-      console.log(error)
-    }
 
+      setMovies(response.data);
+
+    } 
+    catch(err)
+    {
+      console.log(err);
+    }
   }
 
   const getMovieData = async (movieId) => {
-    try{
-      const response = await api.get(`/api/v1/movies/${movieId}`);
-      const singleMovie = response.data;
-      setMovie(singleMovie);
-      setReviews(singleMovie.reviews);
-    } catch (error) {
-    console.log(error);
+     
+    try 
+    {
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
     }
+
   }
 
-
-  // This runs when the component first loads
   useEffect(() => {
     getMovies();
-  }, [])
+  },[])
 
-  // The site would throw an exception if the movies are not loaded yet
-  // So we need to check if the movies are loaded before rendering the page
-  // Homemade solution to the problem, hopefully better solutions exist
-  if (!movies) {
-    return <div>Loading...</div>
-  }
   return (
     <div className="App">
       <Header/>
       <Routes>
-        <Route path="/" element={<Layout/>}>
-            <Route path="/" element={<Home movies={movies}/>} />
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
             <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-            <Route path='/Reviews/:movieId' element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews}/>}></Route>
-        </Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+          </Route>
       </Routes>
-    
+
     </div>
   );
 }
--- react_movie_client/movie-client-v1/src/components/reviewForm/ReviewForm.js
+++ react_movie_client/movie-client-v1/src/components/reviewForm/ReviewForm.js
@@ -1,18 +1,17 @@
-import {Form, Button} from 'react-bootstrap';
+import {Form,Button} from 'react-bootstrap';
 
+const ReviewForm = ({handleSubmit,revText,labelText,defaultValue}) => {
+  return (
 
-const ReviewForm = ({handleSubmit, revText, labelText, defaultValue}) => {
+    <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>{labelText}</Form.Label>
+            <Form.Control ref={revText} as="textarea" rows={3} defaultValue={defaultValue} />
+        </Form.Group>
+        <Button variant="outline-info" onClick={handleSubmit}>Submit</Button>
+    </Form>   
 
-    return (
-        <Form>
-
-            <Form.Group className='mb-3' controlId='exampleForm.ControlTextareal'>
-                <Form.Label>{labelText}</Form.Label>
-                <Form.Control ref={revText} as="textarea" rows={3} defaultValue={defaultValue} />
-            </Form.Group>
-            <Button variant='outline-info' onClick={handleSubmit}>Submit</Button>
-        </Form>
-    )
+  )
 }
 
-export default ReviewForm;
+export default ReviewForm
