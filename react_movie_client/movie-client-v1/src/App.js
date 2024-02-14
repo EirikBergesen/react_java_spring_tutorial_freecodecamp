4b0a226879254c31c09e3296019f36b8b6bd3a0d
EirikBergesen <eirikbr@yahoo.com>
2024-02-14 11:13:03 +0100

    Added review feature, but does not work

--- react_movie_client/movie-client-v1/src/App.js
+++ react_movie_client/movie-client-v1/src/App.js
@@ -6,10 +6,13 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
 
   const getMovies = async () => {
     try {
@@ -20,6 +23,18 @@ function App() {
     }
 
   }
+
+  const getMovieData = async (movieId) => {
+    try{
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+    } catch (error) {
+    console.log(error);
+    }
+  }
+
 
   // This runs when the component first loads
   useEffect(() => {
@@ -39,6 +54,7 @@ function App() {
         <Route path="/" element={<Layout/>}>
             <Route path="/" element={<Home movies={movies}/>} />
             <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path='/Reviews/:movieId' element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews}/>}></Route>
         </Route>
       </Routes>
     
